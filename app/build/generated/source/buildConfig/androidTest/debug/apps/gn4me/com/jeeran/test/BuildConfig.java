f180d6522f0d513fe7629f4f43c011c4e08644b3
monni94 <monni_3000@yahoo.com>
2016-05-18 19:57:28 +0300

    test

---
+++ app/build/generated/source/buildConfig/androidTest/debug/apps/gn4me/com/jeeran/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package apps.gn4me.com.jeeran.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "apps.gn4me.com.jeeran.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
